30b915580935628f9e6498eb9cae570dc0b26fb1
pen0914 <115807679+pen0914@users.noreply.github.com>
2022-12-16 18:44:03 +0900

    コンポーネントと関数の型定義

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,8 @@ import axios from "axios";
 import { useState } from "react";
 import "./styles.css";
 import { Todo } from "./Todo";
-
-type TodoType = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
+import { TodoType } from "./types/todo";
+import { Text } from "./Text";
 
 export default function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
@@ -22,9 +17,11 @@ export default function App() {
 
   return (
     <div className="App">
+      <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
         <Todo
+          key={todo.id}
           title={todo.title}
           userId={todo.userId}
           completed={todo.completed}
--- src/practice/practice.tsx
+++ src/practice/practice.tsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
+
 export const Practice = () => {
+  let plus = [1, 2, 3];
   const calcTotal = (num: number) => {
-    const total = num * 1.1;
-    console.log(total);
+    const total = plus * num;
+    setOne(total);
+
+    const [one, setOne] = useState<Array<number>>([]);
   };
 
-  const onClickTotal = () => calcTotal(1000);
+  const onClickTotal = () => calcTotal(5);
 
   return (
     <div>
       <p>練習問題</p>
       <button onClick={onClickTotal}>計算</button>
+      <p>{`${one}`}</p>
     </div>
   );
 };
